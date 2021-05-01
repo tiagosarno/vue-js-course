f1ed0c3f441e0f5693d9af3a386811576a307d97
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-05-01 12:30:28 -0300

    Utilizando componentes

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">    
+    <app-contadores />
+    <hr />
+    <button>Não quero herdar o estilo do filho (scoped)?</button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
