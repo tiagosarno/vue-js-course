d4278f3db1a58a970bdccfc0464fec0c10b8b747
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-04-30 21:34:32 -0300

    projeto simples Vue CLI

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      {{ titulo }}
+      <button @click="titulo += '#'">Alterar</button>
+  </div>  
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      titulo: 'Curso TOP Vue.js'
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    background-color: cadetblue;
+    color: white;
+    padding: 20px;
+    border-radius: 8px;
+  }
+</style>
