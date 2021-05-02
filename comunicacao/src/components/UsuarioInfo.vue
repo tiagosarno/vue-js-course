cd270c52072ea3b16cd08e090413fed8f18f0f9d
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-05-01 22:40:21 -0300

    Comunicação entre componentes Pai x Filho

---
+++ comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Usuário: {{ nome }}</p>
+        <p>Usuário invertido: {{ inverterNome() }}</p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            required: true,
+            default: 'Tiago',
+            /*
+            default: function() {
+                return 'Rocha'
+            }
+            */
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            this.nome = 'Rocha'
+            
+            // Emitindo um evento customizado..
+            this.$emit('nomeMudou', this.nome)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
