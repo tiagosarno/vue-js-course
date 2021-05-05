7ed8d3eb00a2f8b76e65d478619f9e8bbee90069
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-05-05 12:32:23 -0300

    Utilização do keep-alive para manter estado de um componente dinâmico

---
+++ citacoes-keep-alive-component/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+	<div id="app">
+		<span>
+			<button @click="componente = 'Citacoes'">Citações</button>
+			<button @click="componente = 'Sobre'">Sobre</button>
+		</span>
+		<div id="app">
+			<keep-alive>
+				<component :is="componente" />
+			</keep-alive>
+		</div>
+	</div>
+</template>
+
+<script>
+import Citacoes from './components/Citacoes'
+import Sobre from './components/Sobre'
+
+export default {
+	components: { Citacoes, Sobre },
+	data(){
+		return {
+			// Pode-se alternar dinamicamente entre componentes
+			componente: 'Citacoes'
+		}		
+	}
+}
+</script>
+
+<style>
+	#app {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		font-size: 1.6rem;
+	}
+
+	button {
+        outline: none;
+        padding: 5px 15px 10px;
+        margin: 10px 5px;
+        border-radius: 3px;
+        font-size: 2rem;
+        font-weight: 300;
+        color: #FFF;
+        background-color: rgba(0, 0, 0, .5);
+    }
+</style>
---
+++ citacoes-keep-alive-component/src/components/Citacao.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="citacao">
+        <slot name="fonte"></slot>
+        <slot></slot>
+        <slot name="autor"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log('Componente criado novamente')
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    h2 {
+        color: black;
+    }
+</style>
