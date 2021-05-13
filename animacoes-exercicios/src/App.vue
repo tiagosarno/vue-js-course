2c3dd1d1393dd8da3ec4f0389bd6177b259b7437
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-05-13 17:01:04 -0300

    Animações iniciais

---
+++ animacoes-exercicios/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<hr />
+		<b-button variant="primary" class="mb-4"
+			@click="exibir = !exibir">Mostrar Mensagem</b-button>
+		<transition name="fade">
+			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+		</transition>
+		<transition name="slide" type="animation">
+			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+		</transition>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data(){
+		return {
+			msg: 'Uma mensagem de Tiago Rocha',
+			exibir: false
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
+}
+.fade-enter, .fade-leave-to{
+	opacity: 0;
+}
+.fade-enter-active, .fade-leave-active{
+	transition: opacity 2s;
+}
+
+@keyframes slide-in {
+	from { transform: translateY(40px); }
+	to { transform: translateY(0); }
+}
+@keyframes slide-out {
+	from { transform: translateY(0px); }
+	to { transform: translateY(40px); }
+}
+.slide-enter-active {
+	animation: slide-in 2s ease;
+	transition: opacity 2s;
+}
+.slide-leave-active {
+	animation: slide-out 2s ease;
+	transition: opacity 6s;
+}
+.slide-enter, .slide-leave-to{
+	opacity: 0;
+}
+</style>
