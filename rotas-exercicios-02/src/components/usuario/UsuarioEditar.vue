0aaa7492271c26b9a96a115b9fc969890f4c6c8d
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-05-24 14:00:10 -0300

    Multiplos routers nomeados

---
+++ rotas-exercicios-02/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="usuario-editar">
+      <h3>Usuario Editar</h3>
+      <hr />
+      <p><strong>Código via Props: </strong>{{ id }}</p>
+      <p><strong>Código via $route: </strong>{{ $route.params.id }}</p>
+      <p><strong>Query Param: Completo - </strong>{{ $route.query.completo ? 'Sim' : 'Não' }}</p>
+      <p><strong>Query Param: Lingua - </strong>{{ $route.query.lingua }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  // data() {
+  //   return {
+  //     id: this.$route.params.id,
+  //   };
+  // },
+  // watch: {
+  //   $route(to, from) {
+  //     this.id = to.params.id;
+  //   },
+  // },
+
+}
+</script>
+
+<style>
+
+</style>
