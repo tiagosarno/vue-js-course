6f3b2bf0540bd094885bb54ae3fe5594d51dd57b
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-05-24 14:41:56 -0300

    Animando rotas com animate.css

---
+++ rotas-exercicios-animacoes/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="usuario-detalhe">
+      <h3>Usuario Detalhe</h3>
+      <hr />
+      <p><strong>Código: </strong>{{ id }}</p>
+      <router-link tag="button" primario
+        :to="{ name: 'editarUsuario', params: { id },
+             query: { completo: true, lingua: 'pt-BR'},
+             hash: '#rodape' }">
+      <!-- <router-link tag="button" primario
+        :to="`/usuario/${id}/editar`"> -->
+      <!-- <router-link tag="button" primario
+        :to="`/usuario/${$route.params.id}/editar`"> -->
+          Editar
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  // data() {
+  //   return {
+  //     id: this.$route.params.id,
+  //   };
+  // },
+  // watch: {
+  //   $route(to, from) {
+  //     this.id = to.params.id;
+  //   },
+  // },
+}
+</script>
+
+<style>
+
+</style>
---
+++ rotas-exercicios-animacoes/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="usuario-editar">
+      <h3>Usuario Editar</h3>
+      <hr />
+      <p><strong>Código via Props: </strong>{{ id }}</p>
+      <p><strong>Código via $route: </strong>{{ $route.params.id }}</p>
+      <p><strong>Query Param: Completo - </strong>{{ $route.query.completo ? 'Sim' : 'Não' }}</p>
+      <p><strong>Query Param: Lingua - </strong>{{ $route.query.lingua }}</p>
+      <div id="rodape">
+        <h3>Curso Vue</h3>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  // data() {
+  //   return {
+  //     id: this.$route.params.id,
+  //   };
+  // },
+  // watch: {
+  //   $route(to, from) {
+  //     this.id = to.params.id;
+  //   },
+  // },
+
+}
+</script>
+
+<style>
+  #rodape{
+    margin-top: 1000px;
+  }
+</style>
